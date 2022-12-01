7df45470cd67835cebf995edf0c6dc56bb0438fd
sreeja-chithirala <103505618+sreeja-chithirala@users.noreply.github.com>
2022-12-01 23:48:39 +0530

    Update Q1.c

--- LAB6/Q1.c
+++ LAB6/Q1.c
@@ -1,1 +1,61 @@
+#include<stdio.h>
+#include<string.h>
 
+#define SIZE 100
+
+void E(), E_(), T(), T_(), F();
+
+int i=0;
+
+char s[SIZE];
+
+int main() {
+    printf("\n Enter the string: ");
+    scanf("%s",s);
+    E();
+    if(i==strlen(s)) {
+        printf("\n String successfully parsed.");
+    } else {
+        printf("\n String parsing failed.");
+    }
+}
+
+void E() {
+    T();
+    E_();
+}
+
+void E_() {
+    if(s[i]=='+') {
+        i++;
+        T();
+        E_();
+    }
+}
+
+void T() {
+    F();
+    T_();
+}
+
+void T_() {
+    if(s[i]=='*') {
+        i++;
+        F();
+        T_();
+    }
+}
+
+void F() {
+    if(s[i]=='(') {
+        i++;
+        E();
+        if(s[i]==')') {
+            i++;
+        } 
+    } else if(s[i]=='i') {
+        i++;
+    } else {
+        return;
+    }
+}
